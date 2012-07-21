3a4ce4fc8f0b9050aa7d32081d4020aad49f26dc
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-07-21 10:09:55 +0700

    Build ramdisk into kernel
    Do not use the hackish append-ramdisk-to-kernel now that we can
    build the ramdisk right into the kernel with in-line kernel
    building.
    
    Built for aries-common, adapted to ypg1.

--- shbootimg.mk
+++ shbootimg.mk
@@ -1,9 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
+# Add ramdisk dependencies to kernel
+TARGET_KERNEL_BINARIES: $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET) $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/make_ext4fs $(PRODUCT_OUT)/utilities/erase_image $(PRODUCT_OUT)/utilities/bml_over_mtd
+
 INSTALLED_BOOTIMAGE_TARGET := $(PRODUCT_OUT)/boot.img
-$(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(recovery_ramdisk) $(INSTALLED_RAMDISK_TARGET)  $(PRODUCT_OUT)/utilities/erase_image $(PRODUCT_OUT)/utilities/flash_image $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/make_ext4fs
+$(INSTALLED_BOOTIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET)
 	$(call pretty,"Boot image: $@")
-	$(hide) ./device/samsung/ypg1/mkshbootimg.py $@ $(PRODUCT_OUT)/kernel $(INSTALLED_RAMDISK_TARGET) $(recovery_ramdisk)
+	$(hide) $(ACP) $(INSTALLED_KERNEL_TARGET) $@
 
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(INSTALLED_BOOTIMAGE_TARGET)
 	$(ACP) $(INSTALLED_BOOTIMAGE_TARGET) $@
