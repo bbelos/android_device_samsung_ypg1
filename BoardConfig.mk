ef8d62c6fbbe1ec9c6c398c70df8b77a7f192629
hanthesolo <keemsheeps@bellsouth.net>
2012-08-19 10:58:29 -0500

    tweaked/fixed BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,57 +1,56 @@
-# Samsung YP-G1 compile definitions.
+# Copyright (C) 2007 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-BOARD_PROVIDES_BOOTMODE := true
-
-BOARD_USES_UBOOT := false
-BOARD_USES_UBOOT_MULTIIMAGE := false
+# BoardConfig.mk
+#
+# Product-specific compile-time definitions.
+#
 
 # Set this up here so that BoardVendorConfig.mk can override it
 BOARD_USES_GENERIC_AUDIO := false
 
 BOARD_USES_LIBSECRIL_STUB := true
 
-# Use the non-open-source parts, if they exist
--include vendor/samsung/ypg1/BoardConfigVendor.mk
+# Use the non-open-source parts, if they're present
+-include vendor/samsung/galaxysmtd/BoardConfigVendor.mk
 
-# Processor optimizations
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
-ARCH_ARM_HAVE_TLS_REGISTER := true
-ANDROID_ARM_LINKER := true
 
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
-TARGET_BOARD_PLATFORM := s5pc110
-TARGET_BOARD_PLATFORM_GPU := POWERVR_SGX540_120
-TARGET_BOOTLOADER_BOARD_NAME := s5pc110
-
 TARGET_PROVIDES_INIT := true
-TARGET_PROVIDES_INIT_TARGET_RC := true
+TARGET_BOARD_PLATFORM := s5pc110
+TARGET_BOOTLOADER_BOARD_NAME := s5pc110
 TARGET_RECOVERY_INITRC := device/samsung/ypg1/recovery.rc
 
-# Provide our own libaudio
-TARGET_PROVIDES_LIBAUDIO := true
+
 
 # Releasetools
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/ypg1/releasetools/ypg1_ota_from_target_files
 TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/ypg1/releasetools/ypg1_img_from_target_files
 
-# Camera defines
+# Camera
 USE_CAMERA_STUB := false
 ifeq ($(USE_CAMERA_STUB),false)
-BOARD_SECOND_CAMERA_DEVICE := true
 BOARD_CAMERA_LIBRARIES := libcamera
 endif
-
-# OpenGL stuff
-USE_OPENGL_RENDERER := true
-BOARD_EGL_CFG := device/samsung/ypg1/prebuilt/etc/egl.cfg
-BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/ypg1/recovery/graphics.c
-BOARD_USE_SKIA_LCDTEXT := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
@@ -63,27 +62,21 @@ BOARD_V4L2_DEVICE := /dev/video1
 BOARD_CAMERA_DEVICE := /dev/video0
 BOARD_SECOND_CAMERA_DEVICE := /dev/video2
 
-# XXX
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 268435456
+# FM Radio
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+BOARD_FM_DEVICE := si4709
 
-# Max image/partition sizes
-# XXX: check and fix
-BOARD_BOOTIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00280000)
-BOARD_RECOVERYIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00500000)
-BOARD_SYSTEMIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x07500000)
-BOARD_USERDATAIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x04ac0000)
-# The size of a block that can be marked bad.
-BOARD_FLASH_BLOCK_SIZE := 131072
-
-# Kernel defines
 BOARD_NAND_PAGE_SIZE := 4096 -s 128
-BOARD_KERNEL_BASE := 0x02e00000
-BOARD_KERNEL_CMDLINE := console=ttyFIQ0,115200 init=/init no_console_suspend
+BOARD_KERNEL_BASE := 0x32000000
 BOARD_KERNEL_PAGESIZE := 4096
-# We have two kernels, so this is disabled.  See device.mk.
-# TARGET_PREBUILT_KERNEL := device/samsung/ypg1/kernel
-# XXX: fix this
-KERNEL_SAMSUNG_HACK := true
+BOARD_KERNEL_CMDLINE := console=ttyFIQ0,115200 init=/init no_console_suspend
+#TARGET_PREBUILT_KERNEL := device/samsung/ypg1/kernel
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 7864320
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 196608000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2013265920
+BOARD_FLASH_BLOCK_SIZE := 4096
 
 # WIFI defines
 WPA_SUPPLICANT_VERSION := VER_0_8_X
@@ -96,9 +89,8 @@ WIFI_DRIVER_FW_AP_PATH := "/system/etc/wifi/bcm4329_aps.bin"
 WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4329_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
 WIFI_DRIVER_MODULE_NAME := "dhd"
 
-# USB MTP / UMS
-SAMSUNG_USB_MTP_DEVICE := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun0/file
+# Vold
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
 # Recovery
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -106,12 +98,6 @@ BOARD_RECOVERY_HANDLES_MOUNT := true
 BOARD_USES_BML_OVER_MTD := true
 #BOARD_CUSTOM_RECOVERY_KEYMAPPING:= ../../device/samsung/ypg1/recovery_ui.c
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/ypg1/shbootimg.mk
-TARGET_OTA_ASSERT_DEVICE := YP-G1,ypg1,YP-GB1,yp-gb1
+TARGET_OTA_ASSERT_DEVICE := YP-G1,ypg1
 
-# ICS settings
-DISABLE_DEXPREOPT := true
-COMMON_GLOBAL_CFLAGS += -DMISSING_EGL_EXTERNAL_IMAGE
-COMMON_GLOBAL_CFLAGS += -DMISSING_GRALLOC_BUFFERS
-COMMON_GLOBAL_CFLAGS += -DMISSING_EGL_PIXEL_FORMAT_YV12
 
-BOARD_USE_LEGACY_TOUCHSCREEN := true
