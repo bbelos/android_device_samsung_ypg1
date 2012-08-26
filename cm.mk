ef6002a8a386cde6091107dcb1c2566d9e2b875d
oisis <oisis@o2.pl>
2012-08-27 00:33:22 +0300

    Update PRODUCT_DEVICE

--- cm.mk
+++ cm.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/samsung/ypg1/full_ypg1.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := ypg1
+PRODUCT_DEVICE := YP-G1
 PRODUCT_NAME := cm_ypg1
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := YP-G1
