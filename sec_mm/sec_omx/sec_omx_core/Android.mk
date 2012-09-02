7a1c0a5afb43a8f15e23b49326238f7f2cc50b83
oisis <oisis@o2.pl>
2012-09-02 16:23:45 +0300

    Changed LOCAL_MODULE names from aries to s5pc110

--- sec_mm/sec_omx/sec_omx_core/Android.mk
+++ sec_mm/sec_omx/sec_omx_core/Android.mk
@@ -7,13 +7,13 @@ LOCAL_SRC_FILES := SEC_OMX_Component_Register.c \
 	SEC_OMX_Core.c
 
 
-LOCAL_MODULE := libSEC_OMX_Core.aries
+LOCAL_MODULE := libSEC_OMX_Core.s5pc110
 
 LOCAL_CFLAGS :=
 
 LOCAL_ARM_MODE := arm
 
-LOCAL_STATIC_LIBRARIES := libsecosal.aries libsecbasecomponent.aries
+LOCAL_STATIC_LIBRARIES := libsecosal.s5pc110 libsecbasecomponent.s5pc110
 LOCAL_SHARED_LIBRARIES := libc libdl libcutils libutils
 
 LOCAL_C_INCLUDES := $(SEC_OMX_INC)/khronos \
