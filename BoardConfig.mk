a21278befbe5714e7618053693e67609465a9430
Brian Beloshapka <bbelos@hotmail.com>
2013-04-21 11:14:20 -0400

    Remove redundant optimizations.
    Let this be handled by the source makefiles found
    at build/core/combo/arch/arm/armv7-a-neon.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,10 +17,7 @@ BOARD_USES_LIBSECRIL_STUB := true
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8 -mfpu=neon -mfloat-abi=softfp
 ARCH_ARM_HAVE_TLS_REGISTER := true
-ANDROID_ARM_LINKER := true
 
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
