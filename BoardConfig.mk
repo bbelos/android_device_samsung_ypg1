d054546837d168802a62038ea447aa5c317ca38d
zaclimon <isaacpateau05@gmail.com>
2013-11-16 18:19:23 -0500

    Releasetools: Use releasetools extensions

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,21 @@
-# Samsung YP-G1 compile definitions.
+# Copyright (C) 2007 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# BoardConfigCommon.mk
+#
+# Product-specific compile-time definitions.
+#
 
 # Set this up here so that BoardVendorConfig.mk can override it
 BOARD_USES_GENERIC_AUDIO := false
@@ -27,8 +44,7 @@ TARGET_BOOTLOADER_BOARD_NAME := s5pc110
 TARGET_PROVIDES_LIBAUDIO := true
 
 # Releasetools
-TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/samsung/ypg1/releasetools/ypg1_ota_from_target_files
-TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/samsung/ypg1/releasetools/ypg1_img_from_target_files
+TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/ypg1
 
 # Camera defines
 USE_CAMERA_STUB := false
