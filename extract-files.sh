169d607583e66fc26598e229511597e29c4c30bd
zaclimon <isaacpateau05@gmail.com>
2013-12-08 16:22:18 -0500

    Fix proprietary blobs path extraction in extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -3,7 +3,7 @@
 VENDOR=samsung
 DEVICE=ypg1
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+BASE=../../../vendor/$VENDOR/$DEVICE/
 
 echo "Pulling common files..."
 for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
