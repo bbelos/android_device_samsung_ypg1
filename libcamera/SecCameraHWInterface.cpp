5413306b4d9cda90cde67f1768cbe18b9c72e86c
zaclimon <isaacpateau05@gmail.com>
2014-01-05 13:58:16 -0500

    libcamera: Remove uncommon resolutions
    Removing the strange resolutions (1.5 MP and 2.5 MP) from the camera HAL as
    they don't seem to work properly (weird preview rendering and FC's while
    taking snapshots)

--- libcamera/SecCameraHWInterface.cpp
+++ libcamera/SecCameraHWInterface.cpp
@@ -157,7 +157,7 @@ void CameraHardwareSec::initDefaultParameters(int cameraId)
         p.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES,
               "1024x600,800x600,720x480,176x144");
         p.set(CameraParameters::KEY_SUPPORTED_PICTURE_SIZES,
-              "2048x1536,2048x1232,1600x1200,1600x960,1024x600,800x600");
+              "2048x1536,1600x1200,1024x600,800x600");
     } else {
         p.set(CameraParameters::KEY_SUPPORTED_PREVIEW_SIZES,
               "640x480,320x240,176x144");
